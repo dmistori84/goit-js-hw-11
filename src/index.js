76c8aba6e1437ff3ddeaccc5cb343074d62c987d
dmistori84 <81422874+dmistori84@users.noreply.github.com>
2023-05-29 01:06:51 +0300

    tiki

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { getUrl } from "./api.js";
+import { Notify } from "notiflix";
 
 const refs = {
     form: document.querySelector('.search-form'),
@@ -11,23 +12,31 @@ let squery = "";
 
 refs.form.addEventListener('submit', onSubmit);
 refs.btnLoadMore.addEventListener('click', onLoadMore);
+refs.btnLoadMore.classList.add('is-hidden');
 
 async function onSubmit(ev) {
     ev.preventDefault();
+    refs.btnLoadMore.classList.remove('is-hidden');
     const value = ev.currentTarget.elements.searchQuery.value.trim();
     if (value === "") return;
-    page = 1;
+    page = 1;    
     clearWrapper();
     squery = value;
+    
     const res = await getUrl(value, page); 
+    if (res.length === 0) {
+        return Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+    }
     createMurkup(res);
     refs.form.reset();
 }
 
 async function onLoadMore() {
+    refs.btnLoadMore.setAttribute('disabled', true)
     page += 1;
     const res = await getUrl(squery, page); 
     createMurkup(res);
+    refs.btnLoadMore.removeAttribute('disabled');
 }
 
  function createMurkup(arrHits) {
